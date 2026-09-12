bdc2981a5b071f4b25c2d102fe7bbae0b30fe51e
nguyenphuocnhahung <nguyenphuocnhahungl97@gmail.com>
2026-09-12 09:11:02 +0700

    first commit

---
+++ src/test/java/com/airline_booking_app/AirlineBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airline_booking_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineBookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
