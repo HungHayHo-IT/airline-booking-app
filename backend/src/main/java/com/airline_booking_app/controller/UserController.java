c257b2edb73541be02bfc883a4a627a1c9c44f83
nguyenphuocnhahung <nguyenphuocnhahungl97@gmail.com>
2026-09-12 17:14:05 +0700

    dev user

---
+++ backend/src/main/java/com/airline_booking_app/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.airline_booking_app.controller;
+
+import com.airline_booking_app.dto.Response;
+import com.airline_booking_app.dto.UserDTO;
+import com.airline_booking_app.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+
+    @PutMapping
+    public ResponseEntity<Response<?>> updateMyAccount(@RequestBody UserDTO userDTO){
+        return ResponseEntity.ok(userService.updateMyAccount(userDTO));
+    }
+
+    @GetMapping("/pilots")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'PILOT')")
+    public ResponseEntity<Response<List<UserDTO>>> getAllPilots(){
+        return ResponseEntity.ok(userService.getAllPilots());
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<Response<UserDTO>> getAccountDetails(){
+        return ResponseEntity.ok(userService.getAccountDetails());
+    }
+}
---
+++ backend/src/main/java/com/airline_booking_app/services/impl/UserServiceImpl.java
@@ -0,0 +1,109 @@
+package com.airline_booking_app.services.impl;
+
+import com.airline_booking_app.dto.Response;
+import com.airline_booking_app.dto.UserDTO;
+import com.airline_booking_app.entity.User;
+import com.airline_booking_app.exception.NotFoundException;
+import com.airline_booking_app.repo.UserRepo;
+import com.airline_booking_app.services.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepo userRepo;
+    private final PasswordEncoder passwordEncoder;
+    private final ModelMapper modelMapper;
+
+
+    @Override
+    public User currentUser() {
+
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        return userRepo.findByEmail(email)
+                .orElseThrow(()-> new NotFoundException("User Not Found"));
+    }
+
+
+    @Override
+    @Transactional
+    public Response<?> updateMyAccount(UserDTO userDTO) {
+        log.info("Inside updateMyAccount()");
+
+        log.info(String.valueOf(userDTO));
+
+        User user = currentUser();
+
+        if (userDTO.getName() != null && !userDTO.getName().isBlank()){
+            user.setName(userDTO.getName());
+        }
+
+        if (userDTO.getPhoneNumber() != null && !userDTO.getPhoneNumber().isBlank()){
+            user.setPhoneNumber(userDTO.getPhoneNumber());
+        }
+
+        if (userDTO.getPassword() != null && !userDTO.getPassword().isBlank()){
+            String encodedPassword = passwordEncoder.encode(userDTO.getPassword());
+            user.setPassword(encodedPassword);
+        }
+
+        user.setUpdatedAt(LocalDateTime.now());
+
+        userRepo.save(user);
+
+        return Response.builder()
+                .statusCode(HttpStatus.OK.value())
+                .message("Account Updated Successfully")
+                .build();
+
+
+
+    }
+
+
+    @Override
+    public Response<List<UserDTO>> getAllPilots() {
+        log.info("Inside getAllPilots()");
+
+        List<UserDTO> pilots = userRepo.findByRoleName("PILOT").stream()
+                .map(user -> modelMapper.map(user, UserDTO.class))
+                .toList();
+
+        return Response.<List<UserDTO>>builder()
+                .statusCode(HttpStatus.OK.value())
+                .message(pilots.isEmpty() ? "No pilots Found" : "pilots retrieved succesfully")
+                .data(pilots)
+                .build();
+    }
+
+
+    @Override
+    public Response<UserDTO> getAccountDetails() {
+        log.info("Inside getAccountDetails()");
+
+        User user = currentUser();
+
+        UserDTO userDTO = modelMapper.map(user, UserDTO.class);
+
+        return Response.<UserDTO>builder()
+                .statusCode(HttpStatus.OK.value())
+                .message("success")
+                .data(userDTO)
+                .build();
+    }
+
+
+}
