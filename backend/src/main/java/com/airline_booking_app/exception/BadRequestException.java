353f114f6506bb09740847b5bc8283ec6fae1f29
nguyenphuocnhahung <nguyenphuocnhahungl97@gmail.com>
2026-09-12 12:29:24 +0700

    exception

---
+++ backend/src/main/java/com/airline_booking_app/exception/BadRequestException.java
@@ -0,0 +1,8 @@
+package com.airline_booking_app.exception;
+
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String ex){
+        super(ex);
+    }
+}
---
+++ backend/src/main/java/com/airline_booking_app/exception/NotFoundException.java
@@ -0,0 +1,9 @@
+package com.airline_booking_app.exception;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String ex){
+        super(ex);
+    }
+}
+
