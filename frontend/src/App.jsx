34a2fcafdc36dd18d18a5465633fe371ee764656
nguyenphuocnhahung <nguyenphuocnhahungl97@gmail.com>
2026-09-13 12:28:03 +0700

    up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import Navbar from "./components/common/Navbar";
 import Footer from "./components/common/Footer";
 import RegisterPage from "./components/auth/RegisterPage";
 import LoginPage from "./components/auth/LoginPage";
+import HomePage from "./components/pages/HomePage";
+import FindFlightsPage from "./components/pages/FindFlightsPage";
 
 function App() {
   return (
@@ -13,6 +15,9 @@ function App() {
         <Routes>
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
+
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/flights" element={<FindFlightsPage />} />
         </Routes>
       </div>
       <Footer />
